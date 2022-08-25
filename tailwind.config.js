0457e8d6f9fecf1d442f09cb892bc2529153b10e
Chaoci <charlieischao@gmail.com>
2022-08-25 11:22:00 +0800

    first

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,26 @@
 module.exports = {
   content: ["./app/**/*.{html,ejs}"],
   theme: {
+    container:{
+      center:true,
+      padding:'12px',
+      screens:{
+        '2xl':'1440px'
+      },
+    },
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+    },
+    colors:{
+      'main-bg':'#F0F0F0',
+      'main-orange':'#E6553B'
+    },
     extend: {},
   },
   plugins: [
