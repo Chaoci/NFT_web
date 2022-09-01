29f8cd5ce03d4ab6a8e8f0607c3cdab2133b044e
Chaoci <charlieischao@gmail.com>
2022-09-01 15:06:09 +0800

    Update 2022-09-01T07:06:07.316Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,4 +1,33 @@
 "use strict";
 
-var menu = document.querySelector('.header-mobile-menu');
+var menu = document.querySelector('.header-mobile-menu'); // swiper part
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 2,
+  loop: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: -150,
+    depth: 600,
+    modifier: 1,
+    scale: 0.9,
+    slideShadows: false
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    640: {
+      slidesPerView: 2
+    },
+    320: {
+      slidesPerView: 1
+    }
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
 //# sourceMappingURL=all.js.map
