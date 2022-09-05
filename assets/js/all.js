bda74e8db431642f5c03f3f87f0fb2ed3cec3613
Chaoci <charlieischao@gmail.com>
2022-09-05 13:28:55 +0800

    Update 2022-09-05T05:26:58.840Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,6 @@
 "use strict";
 
-var menu = document.querySelector('.header-mobile-menu'); // swiper part
-
+// swiper part
 var swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
   grabCursor: true,
@@ -30,17 +29,6 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true
   }
 }); //masonry
-// var $grid = $('.grid1').imagesLoaded( function() {
-//   // init Masonry after all images have loaded
-//   $grid.masonry({
-//     itemSelector: '.grid-item',
-//   // use element for option
-//     columnWidth: '.grid-sizer',
-//     percentPosition: true,
-//     horizontalOrder: true,
-//     gutter:20,
-//   });
-// });
 
 var $grid = $('.grid1').masonry({
   itemSelector: '.grid-item',
@@ -51,5 +39,79 @@ var $grid = $('.grid1').masonry({
 
 $grid.imagesLoaded().progress(function () {
   $grid.masonry();
+}); //masonry
+
+var $grid = $('.grid2').masonry({
+  itemSelector: '.grid1-item',
+  percentPosition: true,
+  columnWidth: '.grid1-sizer',
+  gutter: 24
+}); // layout Masonry after each image loads
+
+$grid.imagesLoaded().progress(function () {
+  $grid.masonry();
+});
+"use strict";
+
+$("#menu-bar").click(function () {
+  $("#mobile-menu").slideToggle();
+}); // search bar
+
+$('.header-mobile-search').click(function () {
+  $('#menu-bar').hide();
+  $('.header-logo').hide();
+  $('.glass').hide();
+  $('.search-form').show();
+  $('.left-btn').show();
+  $('.header-mobile-area').addClass('justify-between');
+  $('.header-mobile-area').removeClass('justify-end');
+  $('.header-mobile-area').addClass('items-center');
+  $("#mobile-menu").hide(); //make sure that the menu can be hidden
+});
+$('.left-btn').click(function () {
+  $('.search-form').hide();
+  $('#menu-bar').show();
+  $('.header-logo').show();
+  $('.glass').show();
+  $('.left-btn').hide();
+  $('.header-mobile-area').removeClass('justify-between');
+  $('.header-mobile-area').addClass('justify-end');
+  $('.header-mobile-area').removeClass('items-center'); //make sure that the menu can be show as the function on
+}); //
+
+$(".web").click(function () {
+  $('.web-input-area').slideToggle();
+  $('.up').toggle();
+  $('.down').toggle();
+});
+$(".state").click(function () {
+  $('.state-input-area').slideToggle();
+  $('.state-up').toggle();
+  $('.state-down').toggle();
+});
+$(".price").click(function () {
+  $('.price-input-area').slideToggle();
+  $('.price-up').toggle();
+  $('.price-down').toggle();
+});
+$(".bird-kind").click(function () {
+  $('.bird-input-area').slideToggle();
+  $('.bird-up').toggle();
+  $('.bird-down').toggle();
+});
+$(".flower-area").click(function () {
+  $('.flower-input-area').slideToggle();
+  $('.flower-up').toggle();
+  $('.flower-down').toggle();
+});
+$(".birds-number").click(function () {
+  $('.bird-number-input-area').slideToggle();
+  $('.bird-number-up').toggle();
+  $('.bird-number-down').toggle();
+});
+$(".birds-color").click(function () {
+  $('.bird-color-input-area').slideToggle();
+  $('.bird-color-up').toggle();
+  $('.bird-color-down').toggle();
 });
 //# sourceMappingURL=all.js.map
